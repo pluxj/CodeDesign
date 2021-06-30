493bc9bcdbdd99943afc222fc991fc4040ea3be2
zengzhiyong <zengziyong@mycapital.net>
2021-06-30 15:36:15 +0800

    调整 项目 结构

--- cppDesign/code/src/observe.h
+++ cppDesign/code/src/observe.h
@@ -1,7 +1,7 @@
 #include <string>
 #include <iostream>
 #include <vector>
-
+using namespace std;
 
 class CObservers
 {
@@ -9,6 +9,43 @@ public:
 	CObservers() {}
 	~CObservers() {}
 	virtual void update() = 0;
+};
+
+class PersonObservers :public CObservers
+{
+public:
+	PersonObservers() {}
+	~PersonObservers() {}
+
+	virtual void update()
+	{
+		cout << "this is person observer for update." << endl;
+	}
+};
+
+
+class BusinessObserves : public CObservers
+{
+public:
+	BusinessObserves() {}
+	~BusinessObserves() {}
+
+	virtual void update()
+	{
+		cout << "this is business observer for update." << endl;
+	}
+};
+
+class CoderObservers :public CObservers
+{
+public:
+	CoderObservers() {}
+	~CoderObservers() {}
+
+	virtual void update()
+	{
+		cout << "this is coder observer for update." << endl;
+	}
 };
 
 class WeatherData
--- cppDesign/test/test_observer/test_observer.cpp
+++ cppDesign/test/test_observer/test_observer.cpp
@@ -1,3 +1,31 @@
 #include <iostream>
 #include "../../code/src/observe.h"
 
+int main()
+{
+	WeatherData w;
+
+	PersonObservers *p = new PersonObservers;
+	BusinessObserves *b = new BusinessObserves;
+	CoderObservers *c = new CoderObservers;
+
+	w.registerObserver(p);
+	
+	w.notifyObserver();
+
+	w.registerObserver(b);
+	w.notifyObserver();
+
+	w.registerObserver(c);
+	w.notifyObserver();
+
+
+	w.removeObserver(p);
+	w.removeObserver(b);
+	w.removeObserver(c);
+
+	delete p;
+	delete b;
+	delete c;
+}
+
