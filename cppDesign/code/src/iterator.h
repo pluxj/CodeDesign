76385ed59a5d95a2c130f2327217a20bfef8e557
pluxj <651107734@qq.com>
2021-07-07 18:13:35 +0800

    update

--- cppDesign/code/src/iterator.h
+++ cppDesign/code/src/iterator.h
@@ -17,8 +17,8 @@ public:
 class MenuItem
 {
 public:
-	MenuItem(string name, string description, bool vegetarain, double price):name_(name),description_(description),
-		vegetarain_(vegetarain),price_(price);
+	MenuItem(string name, string description, bool vegetarain, double price) :name_(name), description_(description),
+		vegetarain_(vegetarain), price_(price) {};
 
 	string get_name()
 	{
@@ -45,33 +45,6 @@ private:
 	bool vegetarain_;
 	double price_;
 };
-
-class PancaKeHouseMenu 
-{
-public:
-	PancaKeHouseMenu()
-	{
-		add_item("t1", "pppppp", false, 9.88);
-		add_item("t2", "pppppp", false, 19.88);
-		add_item("t3", "pppppp", false, 29.88);
-		add_item("t4", "pppppp", false, 39.88);
-	}
-
-	void add_item(string name, string description, bool vegetarain, double price)
-	{
-		menulist_.push_back(MenuItem(name, description, vegetarain, price));
-	}
-
-	std::shared_ptr<PancaKeHouseMenuIterator > create_iterator()
-	{
-		std::shared_ptr<PancaKeHouseMenuIterator > ptr(new PancaKeHouseMenuIterator(menulist_));
-		return ptr;
-	}
-
-private:
-	vector<MenuItem> menulist_;
-};
-
 
 class PancaKeHouseMenuIterator :public Iterator<MenuItem>
 {
@@ -100,3 +73,31 @@ private:
 	vector<MenuItem> menulist_;
 	int position;
 };
+
+class PancaKeHouseMenu 
+{
+public:
+	PancaKeHouseMenu()
+	{
+		add_item("t1", "pppppp", false, 9.88);
+		add_item("t2", "pppppp", false, 19.88);
+		add_item("t3", "pppppp", false, 29.88);
+		add_item("t4", "pppppp", false, 39.88);
+	}
+
+	void add_item(string name, string description, bool vegetarain, double price)
+	{
+		menulist_.push_back(MenuItem(name, description, vegetarain, price));
+	}
+
+	std::shared_ptr<PancaKeHouseMenuIterator> create_iterator()
+	{
+		std::shared_ptr<PancaKeHouseMenuIterator> ptr(new PancaKeHouseMenuIterator(menulist_));
+		return ptr;
+	}
+
+private:
+	vector<MenuItem> menulist_;
+};
+
+
